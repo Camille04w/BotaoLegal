ad327f64283d88fa3c20e7cdc4c46fa2e0f4d934
Camille04w <vitoriacamille603@gmail.com>
2024-06-24 17:24:20 +0000

    save

--- botao/components/home.tsx
+++ botao/components/home.tsx
@@ -1,26 +1,33 @@
-import { Text, View, StyleSheet, Image } from 'react-native';
-import Sobre from'./sobre';
-
-export default function HomeScreen() {
-
-  const Clicar = () => {
-    alert("Apertei o botão")
-  }
+import React from 'react'
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.titulo}>
-        <Text style={styles.title}>MapMaster</Text>
-      </View>
-      <View style={styles.corpo}>
-      <Image style={styles.image} source={require('../../assets/images/imagem1.png')} />
-    
-      <Text style={styles.text} >Encontre o seu destino com apenas um toque</Text>
-      <Button title="Ver localização" onPress={Clicar} />
-      </View>
-    </View>
-  );
+export default function App() {
+
+  const [page, setPage] = React.useState('home');
+
+  const renderPage = () => {
+    if (page === 'home'){
+      return (
+        <View style={styles.container}>
+          <View style={styles.title}>
+            <Text style={styles.logo}>MapMaster</Text>
+          </View>
+          <View style={styles.corpo}>
+            <Text style={styles.text} >Encontre o seu destino com apenas um toque</Text>
+            <TouchableOpacity style={styles.button} onPress={()=> setPage('sobre')}>
+              <Text style={styles.button}>Ver localização</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      );
+    } else if(page==='sobre'){
+      return <Sobre />
+    }
+  };
+
+  return <View style={styles.buttonText}>{renderPage()}</View>
+  
 }
 
 const styles = StyleSheet.create({
@@ -28,17 +35,19 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     flex: 1,
     backgroundColor: '#FFF',
-    fontFamily: 'Poppins_400Regular'
+    fontFamily: 'Poppins_400Regular',
+    
 
   },
-  corpo:{
+  corpo: {
     justifyContent: 'flex-start',
     alignItems: 'center',
     flex: 1,
     backgroundColor: '#FFF',
-    fontFamily: 'Poppins_400Regular'
+    fontFamily: 'Poppins_400Regular',
+    margin:30,
   },
-  title: {
+  logo: {
     fontSize: 27,
     fontWeight: 'bold',
     color: '#000',
@@ -59,9 +68,19 @@ const styles = StyleSheet.create({
     padding: 20,
     marginBottom: 80,
   },
-  titulo:{
-  display: 'flex',
-  padding: 40,
-  marginLeft:60,
-}
+  title: {
+    display: 'flex',
+    padding: 40,
+    marginLeft: 60,
+  },
+  button: {
+    width:300,
+    height:100,
+    backgroundColor:'#0047FF',
+
+  },
+  buttonText:{
+
+  },
+
 });
